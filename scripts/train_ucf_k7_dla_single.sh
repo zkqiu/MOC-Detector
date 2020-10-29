297b30a658afc125befa2fc8a624f856dcf2d87e
zkqiu <zhikangqiu@163.com>
2020-10-29 11:13:52 +0800

    init

--- scripts/train_ucf_k7_dla_single.sh
+++ scripts/train_ucf_k7_dla_single.sh
@@ -3,24 +3,25 @@ cd src
 PATH_TO_SAVE_MODEL=../experiment/result_model/K7_rgb_coco_dla_single
 
 GPU=0,1,2,3
-CUDA_VISIBLE_DEVICES=$GPU python3 train.py --K 7 --exp_id Train_K7_rgb_coco_dla_single --rgb_model $PATH_TO_SAVE_MODEL --batch_size 32 --master_batch 8 --lr 5e-4 --gpus $GPU --num_workers 16 --num_epochs 12 --lr_step 6,9 --save_all
+# CUDA_VISIBLE_DEVICES=$GPU python3 train.py --K 7 --exp_id Train_K7_rgb_coco_dla_single --rgb_model $PATH_TO_SAVE_MODEL --batch_size 32 --master_batch 8 --lr 5e-4 --gpus $GPU --num_workers 16 --num_epochs 12 --lr_step 6,9 --save_all
 
 
 # -------------------- Evaluation --------------------
-PATH_TO_RGB_MODEL='K7_rgb_imagenet_resnet18_single/model_[10]_2020-10-21-16-09.pth'
-INFERENCE_DIR='../experiment/result_model/K7_rgb_imagenet_resnet18_single/inference'
+PATH_TO_RGB_MODEL='K7_rgb_coco_dla_single/model_[12]_2020-10-24-06-26.pth'
+PATH_TO_FLOW_MODEL='K7_flow_coco_dla_single/model_[12]_2020-10-26-03-45.pth'
+INFERENCE_DIR='../experiment/result_model/K7_rgb_coco_dla_single/rgb_flow_inference'
 
-# CUDA_VISIBLE_DEVICES=$GPU python3 det.py --task normal --K 7 --gpus $GPU --batch_size 1 --master_batch 1 --num_workers 16 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --inference_dir $INFERENCE_DIR --flip_test --arch resnet_18
+CUDA_VISIBLE_DEVICES=$GPU python3 det.py --task normal --K 7 --gpus $GPU --batch_size 16 --master_batch 4 --num_workers 16 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --flow_model ../experiment/result_model/$PATH_TO_FLOW_MODEL --inference_dir $INFERENCE_DIR --flip_test --ninput 5
 # CUDA_VISIBLE_DEVICES=0 python3 det.py --task normal --K 7 --gpus 0 --batch_size 1 --master_batch 1 --num_workers 0 --rgb_model ../experiment/result_model/$PATH_TO_RGB_MODEL --inference_dir $INFERENCE_DIR --flip_test --arch resnet_18
 #
-# python3 ACT.py --task frameAP --K 1 --th 0.5 --inference_dir $INFERENCE_DIR
+python3 ACT.py --task frameAP --K 1 --th 0.5 --inference_dir $INFERENCE_DIR
 #
-# python3 ACT.py --task BuildTubes --K 1 --inference_dir $INFERENCE_DIR
+python3 ACT.py --task BuildTubes --K 1 --inference_dir $INFERENCE_DIR
 #
-# python3 ACT.py --task videoAP --K 1 --th 0.1 --inference_dir $INFERENCE_DIR
-# python3 ACT.py --task videoAP --K 1 --th 0.2 --inference_dir $INFERENCE_DIR
-# python3 ACT.py --task videoAP --K 7 --th 0.5 --inference_dir $INFERENCE_DIR
-# python3 ACT.py --task videoAP --K 7 --th 0.75 --inference_dir $INFERENCE_DIR
+python3 ACT.py --task videoAP --K 1 --th 0.1 --inference_dir $INFERENCE_DIR
+python3 ACT.py --task videoAP --K 1 --th 0.2 --inference_dir $INFERENCE_DIR
+python3 ACT.py --task videoAP --K 7 --th 0.5 --inference_dir $INFERENCE_DIR
+python3 ACT.py --task videoAP --K 7 --th 0.75 --inference_dir $INFERENCE_DIR
 # python3 ACT.py --task videoAP_all --K 7 --inference_dir $INFERENCE_DIR
 
 cd ..
